b8bed21b94125989b4591d4277094bff47a5a544
mailto1587 <mailto1587@gmail.com>
2015-05-06 18:56:48 +0800

    Added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+VERSION = '0.1'
+
+setup(name='tornado-httpclient-session',
+      version=VERSION,
+      packages=['httpclient_session'],
+      author='mailto1587',
+      author_email='mailto1587@gmail.com',
+      description='Session support to tornado.httpclient.',
+      license='http://opensource.org/licenses/MIT',
+      url='https://github.com/mailto1587/tornado-httpclient-session',
+      keywords=['Tornado', 'HttpClient', 'Session'],
+      install_requires=[
+          'tornado'
+      ])
